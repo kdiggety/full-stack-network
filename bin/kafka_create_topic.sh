372d7616ff5ddc3811e51b13e528747cd4af46e3
Ken Lewis <klewisjr@MACDADDY.lan>
2024-06-20 12:04:17 -0700

    init

--- bin/kafka_create_topic.sh
+++ bin/kafka_create_topic.sh
@@ -1,4 +1,1 @@
-# full-stack-network
-
 kafka-topics --create --topic my-first-topic --bootstrap-server 127.0.0.1:9094
-kafka-topics --list --bootstrap-server 127.0.0.1:9094
---
+++ bin/kafka_list_topics.sh
@@ -0,0 +1,1 @@
+kafka-topics --list --bootstrap-server 127.0.0.1:9094
